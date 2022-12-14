bf4e1899e9a7bce718fc2201b49c81099bc503a6
hi-aa <ost.jj1201@gmail.com>
2022-12-14 18:08:37 +0900

    slot 수정

--- src/components/posts/PostItem.vue
+++ src/components/posts/PostItem.vue
@@ -1,16 +1,8 @@
 <template>
 	<AppCard>
-		<!-- <div class="card">
-			<div class="card-body"> -->
-		<h5 class="card-title">{{ title }}</h5>
-		<p class="card-text">
-			{{ content }}
-		</p>
-		<p class="card-muted">
-			{{ createdAt }}
-		</p>
-		<!-- </div>
-		</div> -->
+		<template v-slot:header>{{ title }}</template>
+		<template>{{ content }}</template>
+		<template v-slot:footer>{{ createdAt }}</template>
 	</AppCard>
 </template>
 
